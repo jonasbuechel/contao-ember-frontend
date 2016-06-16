b6b48a563e699b95bbbb31c6435189389d74ed75
Jonas Büchel <jonas.buechel@gmail.com>
2016-06-16 22:49:06 +0200

    add some service calls to the route

--- app/pods/ueber-mich/route.js
+++ app/pods/ueber-mich/route.js
@@ -1,13 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  'contao-call': Ember.inject.service(),
+
+  model() {
+
+    var myresult = this.get('contao-call').getElementsByArticle(17);
+    //var myresult = this.get('contao-call').getElementById(79);
     
-    ajax: Ember.inject.service(),
+    Ember.Logger.log(myresult);
     
-    model() {
-        var contaoURL = "http://www.selina-buechel.ch/SimpleAjax.php?acid=ajaxrequest&action=contentByArticle&id=17";
-        var ajaxResult = this.get('ajax').request(contaoURL);
-        Ember.Logger.log(ajaxResult);
-        return ajaxResult;
-    }
+    return myresult;
+  }
 });
