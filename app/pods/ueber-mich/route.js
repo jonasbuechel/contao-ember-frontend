c6b0c1a3025ab5d36486f052335ae4626a354ffb
Jonas Büchel <jonas.buechel@gmail.com>
2016-06-16 22:04:58 +0200

    test a simple ajax-call to contao

--- app/pods/ueber-mich/route.js
+++ app/pods/ueber-mich/route.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    
+    ajax: Ember.inject.service(),
+    
+    model() {
+        var contaoURL = "http://www.selina-buechel.ch/SimpleAjax.php?acid=ajaxrequest&action=contentByArticle&id=17";
+        var ajaxResult = this.get('ajax').request(contaoURL);
+        Ember.Logger.log(ajaxResult);
+        return ajaxResult;
+    }
 });
