6f7ec8bb6a88f2dbd3de88fe2b56136899aa5564
Jonas Büchel <jonas.buechel@gmail.com>
2016-06-21 22:54:13 +0200

    added service function to get contao pages

--- app/services/contao-call.js
+++ app/services/contao-call.js
@@ -4,6 +4,13 @@ export default AjaxService.extend({
   host: 'http://www.selina-buechel.ch',
   prefix: 'SimpleAjax.php?auth=qw9a47twbo&',
   
+  getPages(){
+    var contaoURL = this.get('prefix') + "action=pages";
+    var ajaxResult =  this.request(contaoURL);
+    
+    return ajaxResult;
+  },
+  
   getElementsByArticle(articleId){
     var contaoURL = this.get('prefix') + "action=contentByArticle&id=" + articleId;
     var ajaxResult =  this.request(contaoURL);
